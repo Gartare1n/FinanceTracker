aadc1bcb31b3792d5c921dde2d439f354a172f06
Mono <snayper00001@gmail.com>
2022-05-29 13:45:36 +0300

    Stocks search and table with stocks which you tracking

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Finance Tracker
-* ...
+* Stock tracking with IEX api
+* Sign up, log in and account recovery via confirmation email with Devise gem
+* Ability to add stocks in your portfolio
+* Invite to friends list and check portfolio your friends
+* Many-to-Many associations between Users and Stocks
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,20 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol) 
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   secret_token: Rails.application.credentials.iex_client[:secret_access_key],
                                   endpoint: 'https://cloud.iexapis.com/v1')
-    client.quote(ticker_symbol).latest_price
+    begin
+      company = client.company(ticker_symbol)
+      quote = client.quote(ticker_symbol)
+      new(ticker: ticker_symbol, name: company.company_name, last_price: quote.latest_price)
+    rescue => exception
+      return nil
+    end
   end
 
 end
